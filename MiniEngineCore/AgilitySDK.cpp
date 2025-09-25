785009debb754a74f8a263f7f9cd6ef9fb068ef9
cross-word <ss213358ss@gmail.com>
2025-09-26 00:18:39 +0900

    implement: cmake build procedure

--- MiniEngineCore/AgilitySDK.cpp
+++ MiniEngineCore/AgilitySDK.cpp
@@ -1,4 +1,4 @@
 #include "stdafx.h"
 
-extern "C" { __declspec(dllexport) extern const unsigned int  D3D12SDKVersion = 616; }
-extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
+//extern "C" { __declspec(dllexport) extern const unsigned int  D3D12SDKVersion = 616; }
+//extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
